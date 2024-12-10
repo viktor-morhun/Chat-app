d9610bb5621d8450e95b018211aa47be96bba8fa
Viktor Morhun <morhun.fe@gmail.com>
2024-12-10 16:24:59 +0200

    update cors

--- server/app.js
+++ server/app.js
@@ -5,7 +5,12 @@ const messageRoutes = require("./routes/messageRoutes");
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: "https://chat-app-reenbit.vercel.app",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api/chats", chatRoutes);
